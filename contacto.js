3db53319955081d05831bb9579f3d30fac4fc011
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-04-18 17:39:00 -0300

    agrego alerts en formularios con jquery, pongo link en logo nike, agrego imagen logo en navegador

---
+++ contacto.js
@@ -0,0 +1,18 @@
+function alerta(icon, title, text) {
+    Swal.fire({
+        icon: icon,
+        title: title,
+        text: text,
+    })
+}
+$(document).ready(function() {
+    $('#btn-contacto-form').submit(function(event) {
+      event.preventDefault();
+      alerta("", "¡Te registraste!", "Enviamos un correo a tu cuenta para verificarte");
+    });
+    
+    $('#btn-reclamo-form').submit(function(event) {
+      event.preventDefault();
+      alerta("", "Tu reclamo ha sido recibido", "Nos pondremos en contacto solucionarlo");
+    }); 
+});

1eb7ab6b3d29c6bd3ff1c5a43bfd3e1a4ea096bb
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2023-04-21 21:55:48 -0300

    agrego crear pdf

--- contacto.js
+++ contacto.js
@@ -16,3 +16,15 @@ $(document).ready(function() {
       alerta("", "Tu reclamo ha sido recibido", "Nos pondremos en contacto para solucionarlo");
     }); 
 });
+
+$(".btn-pdf").click(function(event){
+  event.preventDefault();
+  var nombre = $("#name").val();
+  var email = $("#email").val();
+  var mensaje = $("#message").val();
+  
+  var pdf = new window.jspdf.jsPDF({orientation: 'p',unit: 'mm',format: 'a5',putOnlyUsedFonts:true})
+
+  pdf.text(`Nombre: ${nombre}\nApellido: ${email}\nEmail: ${mensaje}`, 40, 30);
+  pdf.save(`pdf-creado.pdf`);
+});
